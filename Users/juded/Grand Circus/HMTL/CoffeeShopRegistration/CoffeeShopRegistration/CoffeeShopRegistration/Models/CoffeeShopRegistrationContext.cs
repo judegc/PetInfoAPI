12f29b8bb5e9ce5e193a9bad996657bf33a24007
judegc <juducation@outlook.com>
2022-09-18 20:50:28 -0400

    Finished Assignment

---
+++ Users/juded/Grand Circus/HMTL/CoffeeShopRegistration/CoffeeShopRegistration/CoffeeShopRegistration/Models/CoffeeShopRegistrationContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace CoffeeShopRegistration.Models
+{
+    public partial class CoffeeShopRegistrationContext : DbContext
+    {
+        public CoffeeShopRegistrationContext()
+        {
+        }
+
+        public CoffeeShopRegistrationContext(DbContextOptions<CoffeeShopRegistrationContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<RegisteredUser> RegisteredUsers { get; set; } = null!;
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=localhost;Initial Catalog=CoffeeShopRegistration; Integrated Security=SSPI;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<RegisteredUser>(entity =>
+            {
+                entity.Property(e => e.Email).HasMaxLength(30);
+
+                entity.Property(e => e.FirstName).HasMaxLength(30);
+
+                entity.Property(e => e.LastName).HasMaxLength(30);
+
+                entity.Property(e => e.Password).HasMaxLength(30);
+
+                entity.Property(e => e.TimeStamp).HasColumnType("datetime");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Users/juded/Grand Circus/HMTL/CoffeeShopRegistration/CoffeeShopRegistration/CoffeeShopRegistration/Models/RegisteredUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoffeeShopRegistration.Models
+{
+    public partial class RegisteredUser
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public int? Points { get; set; } = 0;
+        public DateTime TimeStamp { get; set; }
+    }
+}
